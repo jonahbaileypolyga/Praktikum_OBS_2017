0a1d08394ae7fc6a1f10593865f10f85998db309
LonhornHDTV <31323357+LonghornHDTV@users.noreply.github.com>
2018-01-17 21:20:58 +0100

    Fixes and Updates

--- ausb_ein.php
+++ ausb_ein.php
@@ -1,3 +1,63 @@
 <?php
+//CREATE TABLE `obs_prak`.`obs_prak` ( `id` INT NOT NULL AUTO_INCREMENT , `kategorie` VARCHAR(255) NOT NULL , `prak_als` VARCHAR(255) NOT NULL , `name` VARCHAR(255) NOT NULL , `firmenname` VARCHAR(255) NOT NULL , `strasse` VARCHAR(255) NOT NULL , `plz` INT(255) NOT NULL , `stadt` VARCHAR(255) NOT NULL , `telefon` INT(255) NOT NULL , `fax` INT(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `webseite` VARCHAR(255) NOT NULL , `kontaktperson` VARCHAR(255) NOT NULL , `taetigkeit` VARCHAR(255) NOT NULL , `beschreibung` LONGTEXT NOT NULL , PRIMARY KEY (`id`), INDEX (`kategorie`), INDEX (`prak_als`), INDEX (`name`), INDEX (`firmenname`), INDEX (`strasse`), INDEX (`plz`), INDEX (`stadt`), INDEX (`telefon`), INDEX (`fax`), INDEX (`email`), INDEX (`webseite`), INDEX (`kontaktperson`), INDEX (`taetigkeit`)) ENGINE = InnoDB;
+require_once('config.php');
+$conn = mysqli_connect(
+                  $servername,
+                  $username,
+                  $password
+                );
+$mysqldatabase = "'" . $datenbank . "'";
 
+mysqli_set_charset($conn, 'utf8');
+
+if(!$conn->connect_errno) {
+  //Überprüfe eingaben
+  $checkdatabase = "SHOW DATABASES LIKE " . $mysqldatabase;
+  if(!($checkdatabase = $conn->query($checkdatabase))) {
+    print_r($conn);
+    die("QUERY Error: query in ausbildung on show DATABASES: " . $mysqldatabase);
+  }
+  if($checkdatabase->num_rows == 0) {
+    $sql_befehl ="CREATE DATABASE IF NOT EXISTS " . $mysqldatabase;
+    if(mysqli_query($conn, $sql_befehl)) {
+      mysqli_select_db($conn, $datenbank);
+    }else{
+      die("Der MySQL-Benutzer " . $username . " hat nicht genügend Rechte um eine Datenbank zu erstellen.");
+    }
+  }
+  mysqli_select_db($conn, $datenbank);
+
+  $sql_befehl = "SHOW TABLES LIKE 'obs_prak'";
+  if($resultat = mysqli_query($conn, $sql_befehl)) {
+    if($resultat->num_rows == 0) {
+      $sql_befehl = "CREATE TABLE obs_prak ( `id` INT NOT NULL AUTO_INCREMENT , `kategorie` VARCHAR(255) NOT NULL , `prak_als` VARCHAR(255) NOT NULL , `name` VARCHAR(255) NOT NULL , `firmenname` VARCHAR(255) NOT NULL , `strasse` VARCHAR(255) NOT NULL , `plz` INT(255) NOT NULL , `stadt` VARCHAR(255) NOT NULL , `telefon` INT(255) NOT NULL , `fax` INT(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `webseite` VARCHAR(255) NOT NULL , `kontaktperson` VARCHAR(255) NOT NULL , `taetigkeit` VARCHAR(255) NOT NULL , `beschreibung` LONGTEXT NOT NULL , PRIMARY KEY (`id`), INDEX (`kategorie`), INDEX (`prak_als`), INDEX (`name`), INDEX (`firmenname`), INDEX (`strasse`), INDEX (`plz`), INDEX (`stadt`), INDEX (`telefon`), INDEX (`fax`), INDEX (`email`), INDEX (`webseite`), INDEX (`kontaktperson`), INDEX (`taetigkeit`))";
+      if(mysqli_query($conn, $sql_befehl)) {
+        echo("Bitte die Daten nochmal eintragen. Es ist ein Fehler aufgetreten.");
+        die("");
+      }else{
+        die("Der MySQL-Benutzer " . $username . " hat nicht genügend Rechte um eine Datenbank zu erstellen.");
+      }
+    }
+  }else{
+    die("QUERY Error, query in ausbildung on show TABLES");
+  }
+
+  $sql_befehl = "SHOW TABLES LIKE 'acceptfirma'";
+  if($resultat = mysqli_query($conn, $sql_befehl)) {
+    if($resultat->num_rows == 0) {
+      $sql_befehl = "CREATE TABLE `obs_prak`.`accept` ( `ID` TEXT NOT NULL , `sqlbefehl` LONGTEXT NOT NULL )";
+      if(mysqli_query($conn, $sql_befehl)) {
+        echo("Bitte die Daten nochmal eintragen. Es ist ein Fehler aufgetreten.");
+        die("");
+      }else{
+        die("Der MySQL-Benutzer " . $username . " hat nicht genügend Rechte um eine Datenbank zu erstellen.");
+      }
+    }
+  }else{
+    die("QUERY Error, query in ausbildung on show TABLES");
+  }
+
+} else {
+  die('Keine Verbinung möglich. Ist der Benutzer richtig und das Passwort? Fehler: ' . $conn->connect_error);
+}
  ?>
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//Bitte ein Benutzer und ein Password für SQL erstellen.
+$servername = "127.0.0.1"; //Hier kommt die IP von der Datenbank hin.
+$username = "testuser"; //Hier kommt der Benutzer hin. Am besten nicht der Root.
+$password = "ZCnidwns7PC8Zl2V"; //Hier kommt das Password hin von dem Bentzer von der Datenbank.
+$datenbank ="obs_prak"; //Hier kommt der Name von der Datenbank hin.
+?>
